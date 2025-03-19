94eac98f4f59010af5447f4fadec860e5222ddb0
xu.z.brandon@gmail.com <68095440+bxrr@users.noreply.github.com>
2025-03-19 12:45:21 -0500

    iter2

--- tflite_exec.py
+++ tflite_exec.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tflite_runtime.interpreter as tflite
 
 # load the TFLite model and allocate tensors
@@ -10,9 +11,10 @@ output_details = interpreter.get_output_details()
 
 # test the model on random input data
 input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
-# run them model
+# run the model
 interpreter.invoke()
 
 # get_tensor() returns copy of the tensor data
