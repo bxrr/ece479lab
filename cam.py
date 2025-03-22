5d7451011eb8d8295dbc0f5f769aa843b6d671c3
bxrr <xu.z.brandon@gmail.com>
2025-03-22 15:52:18 -0500

    dynamic

--- cam.py
+++ cam.py
@@ -1,39 +1,41 @@
 import cv2
-import picamera
+# import picamera
 import numpy as np
-from mtcnn.mtcnn import MTCNN
+from mtcnn_cv2 import MTCNN
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-import tflite_runtime.interpreter as tflite
+# import tflite_runtime.interpreter as tflite
+import tensorflow as tf
 
-def capture_image():
-    # Instrctor note: this can be directly taken from the PiCamera documentation
-    # Create the in-memory stream
-    stream = io.BytesIO()
-    with picamera.PiCamera() as camera:
-        camera.capture(stream, format='jpeg')
+# def capture_image():
+#     # Instrctor note: this can be directly taken from the PiCamera documentation
+#     # Create the in-memory stream
+#     stream = io.BytesIO()
+#     with picamera.PiCamera() as camera:
+#         camera.capture(stream, format='jpeg')
         
-    # Construct a numpy array from the stream
-    data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+#     # Construct a numpy array from the stream
+#     data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
     
-    # "Decode" the image from the array, preserving colour
-    image = cv2.imdecode(data, 1)
+#     # "Decode" the image from the array, preserving colour
+#     image = cv2.imdecode(data, 1)
     
-    # OpenCV returns an array with data in BGR order. 
-    # The following code invert the order of the last dimension.
-    image = image[:, :, ::-1]
-    return image
+#     # OpenCV returns an array with data in BGR order. 
+#     # The following code invert the order of the last dimension.
+#     image = image[:, :, ::-1]
+#     return image
 
 def detect_and_crop(mtcnn, image):
     detection = mtcnn.detect_faces(image)[0]
     #TODO
     x,y,w,h = detection['box']
     box = image[x:x+w,y:y+h]
-    xs, ys = image.shape
-    new_x = int(xs*1.2)
-    new_y = int(ys*1.2)
-    cropped_image = np.ones(new_x, new_y)
-    cropped_image[new_x-xs, new_y-ys] = box
+    xs = box.shape[0] 
+    ys = box.shape[1]
+    xp = int(xs*0.1)
+    yp = int(ys*0.1)
+    cropped_image = np.ones((xs + 2*xp, ys + 2*yp))
+    cropped_image[xp:-xp, yp:-yp] = box
     return cropped_image
 
 def show_bounding_box(image, bounding_box):
@@ -63,20 +65,26 @@ def run_model(model, face):
     return output_data
 
 
-tfl_file = "./code/model.tflite"
-interpreter = tflite.Interpreter(model_path=tfl_file)
+tfl_file = "./code/resnet.tflite"
+interpreter = tf.lite.Interpreter(model_path=tfl_file)
 interpreter.allocate_tensors()
 
 # process the image of the first person
 input()
 # 1. Read the image
 mtcnn = MTCNN()
-image = capture_image()
+# image = capture_image()
 # 2. Detect and Crop
-cropped_image = detect_and_crop(MTCNN, image)
+image1 = load_image('testface1.jpg')
+image1 = np.array(image1)
+image2 = load_image('testface2.jpg')
+image2 = np.array(image2)
+
+cropped_image = detect_and_crop(mtcnn, image1)
 # 3. Proprocess
 processed_image = pre_process(cropped_image)
 # 4. Run the model
+print(processed_image)
 data1 = run_model(interpreter, processed_image)
 print(data1)
 
@@ -84,9 +92,9 @@ print(data1)
 input()
 # 1. Read the image
 mtcnn = MTCNN()
-image = capture_image()
+# image = capture_image()
 # 2. Detect and Crop
-cropped_image = detect_and_crop(MTCNN, image)
+cropped_image = detect_and_crop(MTCNN, image2)
 # 3. Proprocess
 processed_image = pre_process(cropped_image)
 # 4. Run the model
