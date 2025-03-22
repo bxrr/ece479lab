15b6d7aebb533b681eebbe38168b017247389a48
bxrr <xu.z.brandon@gmail.com>
2025-03-22 16:32:07 -0500

    tflite fix

--- tflite_exec.py
+++ tflite_exec.py
@@ -1,6 +1,6 @@
 import time
 import numpy as np
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 from PIL import Image
 
 def get_scores(interpreter):
@@ -22,13 +22,8 @@ def get_scores(interpreter):
 
   return output_data.copy()
 
-<<<<<<< HEAD
 # load the TFLite model and allocate tensors
-interpreter = tflite.Interpreter(model_path='L2P2model_fq.tflite')
-=======
-# load the tf.lite model and allocate tensors
-interpreter = tf.lite.Interpreter(model_path='fullint.tflite')
->>>>>>> 69bebabd25bd8ec85cdb129fca40d484c366f841
+interpreter = tflite.Interpreter(model_path='dynamic.tflite')
 interpreter.allocate_tensors()
 
 # get input and output tensors
@@ -38,9 +33,9 @@ output_details = interpreter.get_output_details()
 # test the model on random input data
 input_shape = input_details[0]['shape']
 # input_data = np.array(np.random.random_sample(input_shape) * 255, dtype=np.int8)
-image = Image.open('shoe_test.png').resize((28,28), Image.LANCZOS)
+image = Image.open('pants_test.jpg').convert('L').resize((28,28), Image.LANCZOS)
 image = np.resize(image, (input_shape))
-image = image.astype(np.int8)
+image = image.astype(np.float32)
 #print(image)
 interpreter.set_tensor(input_details[0]['index'], image)
 
